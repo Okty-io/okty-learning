d7af9566cad34b9135156786c30c3fd2b2fdbbc8
Laurent Bassin <laurent@bassin.info>
2019-06-23 19:26:06 +0200

    Add navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <div>
-      <router-outlet #outlet="outlet"></router-outlet>
-    </div>
-  `,
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    template: `
+        <app-navbar></app-navbar>
+        <div style="min-height: calc(100% - 427px)">
+            <router-outlet #outlet="outlet"></router-outlet>
+        </div>
+        <app-footer></app-footer>
+    `,
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { ApiService } from './core/services/api.service';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
-  providers: [
-    ApiService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        CoreModule
+    ],
+    providers: [
+        ApiService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
