92ae23f0544611504564d0946bd3cfa1f1c94d20
Laurent Bassin <laurent@bassin.info>
2019-06-29 20:14:52 +0200

    Convert markdown into HTML

--- src/app/modules/learning/components/lesson-step/lesson-step.component.ts
+++ src/app/modules/learning/components/lesson-step/lesson-step.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import Step from '../../models/step';
+import { MarkdownService } from "ngx-markdown";
 
 @Component({
     selector: 'app-learning-lesson-step',
     templateUrl: './lesson-step.component.html',
     styleUrls: ['./lesson-step.component.scss']
 })
-export class LessonStepComponent {
+export class LessonStepComponent implements OnInit {
 
     @Input() step: Step;
+
+    public markdown: string;
+
+    constructor(private markdownService: MarkdownService) {
+    }
+
+    ngOnInit(): void {
+        this.markdown = this.markdownService.compile(this.step.text);
+    }
 
     completeStep(event: boolean): void {
         this.step.validated = event;
     }
-
 }
