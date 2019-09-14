70071ce8328a6e980a7f848d010887b6c84fe809
Laurent Bassin <laurent@bassin.info>
2019-09-14 15:04:05 -0400

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import Home from "./containers/Home";
 
 function App() {
-  return (
-    <p>Okty Learning</p>
-  );
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
