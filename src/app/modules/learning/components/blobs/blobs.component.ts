5c048afa84c8cfc93944d88ae1a16c7e4b0b431c
Laurent Bassin <laurent@bassin.info>
2019-09-14 14:45:57 -0400

    Remove angular & add react

--- src/app/modules/learning/components/blobs/blobs.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-learning-blobs',
-  templateUrl: './blobs.component.html',
-  styleUrls: ['./blobs.component.scss']
-})
-export class BlobsComponent {
-}
