5aabc432c31a95e91c538bea1f746a3d5548d619
Laurent Bassin <laurent@bassin.info>
2019-09-18 11:30:39 -0400

    Add right panel structure

--- src/components/home/RightPanel.js
+++ src/components/home/RightPanel.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import illustration from '../../assets/home/drawing_illustration.svg'
+import style from './RightPanel.module.scss'
 
 export default () => (
     <div>
+        <div className={style.blobs} style={{display: 'none'}}>
+            <div className={style.main}>
+                <img src={illustration} alt=""/>
+            </div>
 
+            <div className={style.blob}/>
+            <div className={style.blob}/>
+            <div className={style.blob}/>
+        </div>
     </div>
 )
